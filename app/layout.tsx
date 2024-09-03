40c03ba06bab96b43c3f8ad547a1c8ab906371e8
daniel1014 <chuenlik@hotmail.com>
2024-09-03 11:21:58 +0100

    add google analytics to track traffic

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,61 @@
+'use client';
+
 import './globals.css'
-import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { useEffect } from 'react'
+import Script from 'next/script'
+import { metadata } from './metadata'; // Import metadata from the new file
 
 const inter = Inter({ subsets: ['latin'] })
-
-export const metadata: Metadata = {
-  title: 'Daniel Wong - Portfolio',
-  description: 'AI Engineer | Data Scientist | Innovation Enthusiast',
-}
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      window.gtag('config', 'G-X34X66F8W8', {
+        page_path: url,
+      });
+    };
+
+    // Listen for route changes
+    const handleRouteChangeComplete = (url: string) => {
+      handleRouteChange(url);
+    };
+
+    // Add event listener for route changes
+    window.addEventListener('popstate', handleRouteChangeComplete);
+    return () => {
+      window.removeEventListener('popstate', handleRouteChangeComplete);
+    };
+  }, []);
+
   return (
     <html lang="en">
+      <head>
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-G-X34X66F8W8`}
+        />
+        <Script
+          id="google-analytics-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-X34X66F8W8', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
---
+++ app/metadata.ts
@@ -0,0 +1,6 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Daniel Wong - Portfolio',
+  description: 'AI Engineer | Data Scientist | Innovation Enthusiast',
+};
