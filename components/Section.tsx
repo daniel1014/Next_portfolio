bd60f75b48f45ffaf6718f8116f3a7f658637f58
daniel1014 <chuenlik@hotmail.com>
2024-10-08 16:30:06 +0100

    add fade-in animation on each section

--- components/Section.tsx
+++ components/Section.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React from "react";
 
-const Section: React.FC<{ title: string; children: React.ReactNode; id: string }> = ({ title, children, id }) => (
+const Section: React.FC<{
+  title: string;
+  children: React.ReactNode;
+  id: string;
+}> = ({ title, children, id }) => (
   <div className="mb-20" id={id}>
-    <h2 className="text-3xl font-bold mb-8 text-blue-400">{title}</h2>
-    {children}
+    <h2 className="text-3xl font-bold mb-8 text-blue-400 animate-fade-in-delay">
+      {title}
+    </h2>
+    <div className="bg-gray-800 rounded-lg shadow-xl p-8 hover:shadow-2xl transition duration-300 animate-fade-in-delay">
+      {children}
+    </div>
   </div>
 );
 
