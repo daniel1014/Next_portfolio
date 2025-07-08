976a2af5de7f8dc3eee660a71ccd8134a2449890
Daniel Wong Mac <chuenlik@hotmail.com>
2025-07-08 12:53:38 +0100

    Enhance Next.js configuration by adding image optimization settings for improved performance and security; update TrilloDocs image file

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 import {withSentryConfig} from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    formats: ['image/webp', 'image/avif'],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    minimumCacheTTL: 60,
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
+};
 
 export default withSentryConfig(nextConfig, {
 // For all available options, see:
