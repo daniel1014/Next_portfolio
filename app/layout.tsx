eb55685a616e677f5ddce7a4d8c05ee9e60e6779
Daniel Wong Mac <chuenlik@hotmail.com>
2025-08-20 13:38:41 +0100

    Update layout metadata to include new favicon sizes for cross-browser safety

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,9 @@ export const metadata: Metadata = {
   manifest: '/manifest.json',
   icons: {
     icon: [
-      { url: '/favicon.ico' },
+      { url: '/favicon.ico', sizes: '48x48'  },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
     ],
     apple: [{ url: '/apple-icon.png', sizes: '180x180' }],
   },
